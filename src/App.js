b0e3cf85a21b249cc2099d5246d53d413761f53d
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-02 22:16:39 +0100

    advice

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends React.Component {
-  state = { advice: ''}
+  state = { advice: "" };
 
   componentDidMount() {
     this.fetchAdvice();
   }
 
   fetchAdvice = () => {
-    axios.get('https://api.adviceslip.com/advice')
-      .then((response)=>{
-        const {advice} = response.data.slip;
-        this.setState({advice});
-        console.log('oya na')
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+        console.log(response);
       })
-      .catch((error)=>{
+      .catch((error) => {
         console.log(error);
       });
-  }
+  };
 
-  render(){
-    const {advice} = this.state;
+  render() {
+    const { advice } = this.state;
 
-    return(
+    return (
       <div className="app">
         <div className="card">
           <h1 className="heading">{advice}</h1>
-          <button className='button' onClick={this.fetchAdvice}>
+          <button
+            className="button"
+            onClick={this.fetchAdvice}
+          >
             <span>GIVE ME ADVICE</span>
           </button>
         </div>
       </div>
-    )
+    );
   }
 }
- 
-export default App ;
+
+export default App;
